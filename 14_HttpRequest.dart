968b0eeeaee4f8aaf5adcf7abff18fb0c347e1b0
Hanyuu <1183580621@qq.com>
2023-05-15 12:53:26 +0800

    v1

---
+++ 14_HttpRequest.dart
@@ -0,0 +1,17 @@
+import 'dart:html';
+
+// void main() async {
+//   String pageHtml = await HttpRequest.getString('www.baidu.com');
+//   print(pageHtml);
+//   // Do something with pageHtml...
+
+//   var request = HttpRequest();
+//   request
+//     ..open('POST', url)
+//     ..onLoadEnd.listen((e) => requestComplete(request))
+//     ..send(encodedData);
+// }
+
+void Sending_data_to_the_server(){
+  
+}
