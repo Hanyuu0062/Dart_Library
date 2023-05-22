5cd7b167e0ba15fab534fc1801b5256e92accfc3
Hanyuu 1 <1183580621@qq.com>
2023-05-22 02:49:24 +0000

    v3

--- 14_HttpRequest.dart
+++ 14_HttpRequest.dart
@@ -1,17 +1,40 @@
 import 'dart:html';
 
-// void main() async {
-//   String pageHtml = await HttpRequest.getString('www.baidu.com');
-//   print(pageHtml);
-//   // Do something with pageHtml...
+void main() async {
 
-//   var request = HttpRequest();
-//   request
-//     ..open('POST', url)
-//     ..onLoadEnd.listen((e) => requestComplete(request))
-//     ..send(encodedData);
-// }
-
-void Sending_data_to_the_server(){
-  
 }
+
+void requewst_and_websoket() async{
+    var url = 'https://www.bilibili.com/';
+  // 获取网页原码
+  String pageHtml = await HttpRequest.getString(url);
+  print(pageHtml);
+
+  // Sending data to the server
+  var request = HttpRequest();
+  const data = {'dart': 'fun', 'angular': 'productive'};
+  request
+    ..open('POST', url)
+    ..setRequestHeader(
+      'Content-type',
+      'application/x-www-form-urlencoded',
+    )
+    ..send(encodeMap(data));
+  await request.onLoadEnd.first;
+
+  if (request.status == 200) {
+    // Successful URL access...
+  }
+  // ···
+
+  //Sending and receiving real-time data with WebSockets
+  var ws = WebSocket('ws://echo.websocket.org');
+  ws.send('Hello from Dart!');
+  ws.onMessage.listen((MessageEvent e) {
+    print('Received message: ${e.data}');
+  });
+}
+
+String encodeMap(Map<String, String> data) => data.entries
+    .map((e) => '${Uri.encodeComponent(e.key)}=${Uri.encodeComponent(e.value)}')
+    .join('&');
